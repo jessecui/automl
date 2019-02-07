a6653627eb3a15ffed015a426b6b4a2b77b68eba
Jesse Cui <jessecui@seas.upenn.edu>
2019-02-07 09:09:00 -0500

    fixed print typo

--- starter.py
+++ starter.py
@@ -7,4 +7,4 @@ X_train, X_test, y_train, y_test =    sklearn.model_selection.train_test_split(X
 automl = autosklearn.classification.AutoSklearnClassifier()
 automl.fit(X_train, y_train)
 y_hat = automl.predict(X_test)
-rint("Accuracy score", sklearn.metrics.accuracy_score(y_test, y_hat))
+print("Accuracy score", sklearn.metrics.accuracy_score(y_test, y_hat))
